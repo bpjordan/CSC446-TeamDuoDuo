6c419223cf281d86eff3ad3dcbe643fbc4ebdef1
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2022-12-10 23:40:54 +0000

    Implemented database connection and query endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,23 @@
 #[macro_use] extern crate rocket;
+
 use rocket::fs::FileServer;
+use rocket::serde::{Serialize, Deserialize, json::Json};
+use rocket_db_pools::{Database, Connection};
+use rocket_db_pools::sqlx::{self, Row};
+
+use rocket::futures::{stream::TryStreamExt};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct User {
+    username: String,
+    password: String,
+    email: String,
+}
+
+#[derive(Database)]
+#[database("maindb")]
+struct SiteDB(sqlx::MySqlPool);
 
 #[get("/")]
 fn index() -> &'static str {
@@ -7,15 +25,22 @@ fn index() -> &'static str {
 }
 
 #[get("/")]
-fn raw_query() -> String {
+async fn raw_query(mut db: Connection<SiteDB>) -> Option<Json<Vec<User>>> {
 
-    "Hello, World".to_string()
+    let users = sqlx::query("SELECT * FROM users;")
+    .fetch(&mut *db)
+    .map_ok(|r| User{username: r.get("username"), password: r.get("password"), email: r.get("email") })
+    .try_collect::<Vec<_>>()
+    .await.ok()?;
 
+    Some(Json(users))
 }
 
 #[launch]
 fn rocket() -> _ {
+
     rocket::build()
+    .attach(SiteDB::init())
     .mount("/", FileServer::from("/app/static"))
     .mount("/query", routes!(raw_query))
     .mount("/api", routes!(index))
