bd180f4f07f1bd10432b697576f3140ce9774780
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2022-12-12 21:04:05 +0000

    Made HTTP error for query endpoint more helpful

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate rocket;
 
 use rocket::fs::FileServer;
+use rocket::http::Status;
 use rocket::serde::{Serialize, Deserialize, json::Json};
 use rocket_db_pools::{Database, Connection};
 use rocket_db_pools::sqlx::{self, Row};
@@ -25,15 +26,15 @@ fn index() -> &'static str {
 }
 
 #[get("/query")]
-async fn raw_query(mut db: Connection<SiteDB>) -> Option<Json<Vec<User>>> {
+async fn raw_query(mut db: Connection<SiteDB>) -> Result<Json<Vec<User>>, (Status, &'static str)> {
 
     let users = sqlx::query("SELECT * FROM users;")
     .fetch(&mut *db)
     .map_ok(|r| User{username: r.get("username"), password: r.get("password"), email: r.get("email") })
     .try_collect::<Vec<_>>()
-    .await.ok()?;
+    .await.map_err(|_| (Status::InternalServerError, "Database query failed"))?;
 
-    Some(Json(users))
+    Ok(Json(users))
 }
 
 #[launch]
