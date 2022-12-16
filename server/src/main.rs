9b5bb65b09b7ad197e2c269f14d6f5de6b7924ac
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2022-12-16 01:01:52 +0000

    refactored & added error logging

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use] extern crate rocket;
 extern crate argon2;
 
-use argon2::{Argon2, PasswordVerifier, PasswordHash};
+use std::fmt::Debug;
+
+use argon2::{Argon2, PasswordVerifier, PasswordHash, password_hash::Error::Password};
 
 use rocket::fs::FileServer;
 use rocket::http::Status;
@@ -11,6 +13,29 @@ use rocket_db_pools::{Database, Connection};
 use rocket_db_pools::sqlx::{self, Row};
 
 use rocket::futures::stream::TryStreamExt;
+
+/// Creates a closure to log an error and transform it to a 500 status code.
+/// 
+/// Meant to be used as an argument to `Result::map_err`.
+/// 
+/// Example:  
+/// ```rust,no_run
+/// use std::fs::read_to_string;
+/// s = read_to_string("doc.txt").map_err(log_server_err("reading doc.txt"));
+/// assert!(s, Status::InternalServerError);
+/// ```
+/// 
+/// Output:
+/// ```text
+///    !! Error reading doc.txt: Not Found
+/// ```
+// 
+fn log_server_err<F: Debug>(process: &'static str) -> impl FnOnce(F) -> Status {
+    move |e| {
+        println!("   !! Failed to {}: {:?}", process, e);
+        Status::InternalServerError
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -25,13 +50,13 @@ struct User {
 struct SiteDB(sqlx::MySqlPool);
 
 #[get("/query")]
-async fn raw_query(mut db: Connection<SiteDB>) -> Result<Json<Vec<User>>, (Status, &'static str)> {
+async fn raw_query(mut db: Connection<SiteDB>) -> Result<Json<Vec<User>>, Status> {
 
     let users = sqlx::query("SELECT * FROM users;")
     .fetch(&mut *db)
     .map_ok(|r| User{username: r.get("username"), password: r.get("password"), email: r.get("email") })
-    .try_collect::<Vec<_>>()
-    .await.map_err(|_| (Status::InternalServerError, "Database query failed"))?;
+    .try_collect::<Vec<_>>().await
+    .map_err(log_server_err("reading from database"))?;
 
     Ok(Json(users))
 }
@@ -49,23 +74,25 @@ async fn login_user(mut db: Connection<SiteDB>, username: &str, password: &str) 
     let hashslinginghasher = Argon2::default();
 
     let entry = sqlx::query("SELECT password FROM users WHERE username = ?").bind(username)
-    .fetch_optional(&mut *db).await;
+    .fetch_optional(&mut *db).await.map_err(log_server_err("fetching user from database"))?;
 
     let pwhash = match &entry {
-        Ok(Some(row)) => Ok(row.get(0)),
-        Ok(None) => Ok("$argon2id$v=19$m=4096,t=3,p=1$7cw5ikPiUevZbFw3Oc67GQ$5BfCIWW7v4pYqqHkaDtF2H7G7TXts9+EX69KW5kCyMA"), //hash for an empty string. This ensures the algorithm is still run to dissuade timing attacks
-        Err(_) => Err(Status::InternalServerError),
+        Some(row) => Ok(row.get(0)),
+        None => Ok("$argon2id$v=19$m=4096,t=3,p=1$7cw5ikPiUevZbFw3Oc67GQ$5BfCIWW7v4pYqqHkaDtF2H7G7TXts9+EX69KW5kCyMA"), //hash for an empty string. This ensures the algorithm is still run to dissuade timing attacks
     }?;
 
     let hash_result = hashslinginghasher.verify_password(
         password.as_bytes(),
-        &PasswordHash::new(pwhash).map_err(|_| Status::InternalServerError)?
+        &PasswordHash::new(pwhash).map_err(log_server_err("parsing hash from database"))?
     );
 
     match hash_result {
         Ok(()) => Ok(Status::Ok),
-        Err(argon2::password_hash::Error::Password) => Ok(Status::Unauthorized),
-        _ => Err(Status::InternalServerError)
+        Err(e) => match e {
+            Password => Ok(Status::Unauthorized),
+            _ => Err(Status::InternalServerError)
+        },
+
     }
 
 }
