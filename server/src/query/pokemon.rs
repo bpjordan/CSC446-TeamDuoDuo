b20be0f5472e11e3a9915a31ab90a05082417b62
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-01-19 10:25:52 -0600

    Implemented all remaining API endpoints

--- server/src/query/pokemon.rs
+++ server/src/query/pokemon.rs
@@ -1,21 +1,46 @@
 
-use rocket::{http::Status, serde::{json::Json, Serialize}};
+use rocket::{http::Status, serde::{json::Json, Serialize}, futures::TryStreamExt};
 use rocket_db_pools::Connection;
+use sqlx::FromRow;
 
 use crate::{auth, db};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, FromRow)]
 #[serde(crate = "rocket::serde")]
-pub struct Pokemon {}
+pub struct Pokemon {
+    name: String,
+    r#type: String,
+    trainer: String,
+    sprite: String,
+    image: String
+}
 
 #[get("/pokemon")]
-pub async fn query_pokemon(_s: auth::ProfessorSession, _db: Connection<db::Users>) -> Result<Json<Vec<Pokemon>>, Status> {
+pub async fn query_pokemon(_s: auth::ProfessorSession, mut db: Connection<db::Users>) -> Result<Json<Vec<Pokemon>>, Status> {
 
-    Err(Status::NotImplemented)
+    let pokemon = sqlx::query_as(" SELECT * FROM pokemon")
+    .fetch(&mut *db)
+    .try_collect::<Vec<_>>().await
+    .or_else(|e| {
+        println!("   !! Got error {e}");
+        Err(Status::InternalServerError)
+    })?;
+
+    Ok(Json(pokemon))
+
 }
 
 #[get("/user_pokemon")]
-pub async fn query_user_pokemon(_user_session: auth::UserSession, _db: Connection<db::Users>) -> Result<Json<Vec<Pokemon>>, Status> {
+pub async fn query_user_pokemon(session: auth::TrainerSession, mut db: Connection<db::Users>) -> Result<Json<Vec<Pokemon>>, Status> {
 
-    Err(Status::NotImplemented)
+    let pokemon = sqlx::query_as(" SELECT * FROM pokemon WHERE trainer = ?")
+    .bind(&session.user)
+    .fetch(&mut *db)
+    .try_collect::<Vec<_>>().await
+    .or_else(|e| {
+        println!("   !! Got error {e}");
+        Err(Status::InternalServerError)
+    })?;
+
+    Ok(Json(pokemon))
 }
