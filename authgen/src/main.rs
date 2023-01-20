7cdd049aae85e7f893012ac91a03c83492f4bf8d
Brendan Guillory <brendanguillory@gmail.com>
2023-01-19 21:07:41 -0600

    Implemented 2FA code library and generator we’reくコ:彡 entering squid territory
     くコ:彡                                                                       くコ:彡 
    
                                くコ:彡  くコ:彡
    
                                                           くコ:彡
    
                              くコ:彡 くコ:彡
    
    now C:≡ approaching octopus territory
    
    C:≡                                                           C:≡                               C:≡      C:≡                                              C:≡                         C:≡
    
    onwards C{≡ to jellyfish territory
    
    C{≡             C{≡
    
                C{≡               C{≡                                                     C{≡                         C{≡ C{≡                                 C{≡

--- authgen/src/main.rs
+++ authgen/src/main.rs
@@ -1,6 +1,16 @@
+use mfalib;
 
 static _SAMPLE_BASE64_USER_SECRET: &str = "gzBsRiEnc3Kwc/26S3gklyz5M4UUOztqbO4pbhtgDi4=";
 
 fn main() {
-    println!("Hello, world!");
+    println!("Code: {}", mfalib::gen_mfa(
+            // cap length to 32 bytes
+            _SAMPLE_BASE64_USER_SECRET[..32]
+            // &str -> [u8]
+            .as_bytes()
+            // [u8] -> [u8; 32]
+            .try_into()
+            // I promise this won't fail
+            .unwrap()
+    ));
 }
--- mfalib/src/lib.rs
+++ mfalib/src/lib.rs
@@ -1,13 +1,49 @@
+extern crate crypto;
+
+use chrono;
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
+const MFA_CODE_LENGTH: usize = 6;
+
 pub fn gen_mfa(_user_secret: &[u8; 32]) -> String {
-    todo!()
+    // MD5 can be apparently cracked by a cell phone in 30s
+    // Hopefully we generate our code after 30s!
+    let mut super_secure_md5 = Md5::new();
+
+    // current UTC time
+    // we simply don't put the seconds in the string
+    let time_in_england = chrono::offset::Utc::now();
+    let mut formatted_bad_teeth_time = time_in_england
+        .format("%Y%m%d%H%M")
+        .to_string();
+
+    // Add user secret to UTC time string
+    formatted_bad_teeth_time.push_str(
+        &String::from_utf8(_user_secret.iter().cloned().collect())
+        .unwrap());
+
+    // hash string
+    super_secure_md5.input(formatted_bad_teeth_time.as_bytes());
+
+    // collect the digits from the hash
+    let mut hash_only_numbers = String::new();
+    for hash_char in super_secure_md5.result_str().chars() {
+        if hash_char.is_numeric() {
+            hash_only_numbers.push(hash_char);
+        }
+    }
+
+    // return the first MFA_CODE_LENGTH characters
+    return String::from(&hash_only_numbers[..MFA_CODE_LENGTH]);
 }
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
+    use super::*;
 
     #[test]
     fn it_works() {
-        todo!()
+        gen_mfa(&[b'0'; 32]);
     }
 }
